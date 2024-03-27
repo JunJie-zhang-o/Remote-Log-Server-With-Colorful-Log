abead011a5426aecc607a42744071bbca6449c5a
Jay <jay.zhangjunjie@outlook.com>
2024-03-28 00:19:38 +0800

    其他修改内容，目前无实际意义

--- src/extension.ts
+++ src/extension.ts
@@ -3,10 +3,10 @@ import { RemoteLogServer } from './remote_log_server'
 
 
 
-
+const rls:RemoteLogServer = new RemoteLogServer()
 
 export function activate(context: vscode.ExtensionContext) {
-	let rls = new RemoteLogServer()
+
 	context.subscriptions.push(vscode.commands.registerCommand('RemoteLogServerWithColorfulLog.start_rls', () => { rls.start() }))
 	context.subscriptions.push(vscode.commands.registerCommand('RemoteLogServerWithColorfulLog.stop_rls', () => { rls.stop() }))
 	context.subscriptions.push(vscode.commands.registerCommand('RemoteLogServerWithColorfulLog.toggle_rls', () => { rls.toggle() }))
