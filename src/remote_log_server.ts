965beec28a18f09aa179fc76118161e5eb266467
Jay <jay.zhangjunjie@outlook.com>
2024-03-28 00:17:31 +0800

    🎈 perf(remote_log_server): 优化启动监听逻辑
    当启动监听成功后，修改图标，监听失败不修改图标，避免启动失败后需要关闭在启动

--- src/remote_log_server.ts
+++ src/remote_log_server.ts
@@ -1,5 +1,6 @@
 import * as net from 'net'
 import * as vscode from 'vscode'
+const EventEmitter = require('events');
 
 export class RemoteLogServer {
 	enabled: boolean
@@ -19,7 +20,6 @@ export class RemoteLogServer {
 		this.statusBarItem.show()
 
 		this.outputChannel = vscode.window.createOutputChannel("TCP Log Server Output", "Colorful Log")
-		this.outputChannel.show()
 
 		this.server = net.createServer({ allowHalfOpen: true }, (socket: net.Socket) => {
 			socket.setEncoding('utf8')
@@ -53,17 +53,22 @@ export class RemoteLogServer {
 		let conf = vscode.workspace.getConfiguration('RemoteLogServerWithColorfulLog')
 		let host:string = conf.get("host") as string
 		let port:number = conf.get("port") as number
-		this.statusBarItem.command = "RemoteLogServerWithColorfulLog.stop_rls"
-		this.statusBarItem.text = "Remote Log Server: $(debug-stop)"
-		this.statusBarItem.tooltip = `Click the button to stop monitoring the log.\nHost:${host} Port:${port}`
+
 		this.server.listen(port, host, () => {
 			// vscode.window.setStatusBarMessage(`Log server started: ${JSON.stringify(this.server.address())}`, 3000)
 			vscode.window.showInformationMessage(`The Remote Log Server Started: ${JSON.stringify(this.server.address())}`)
 			this.outputChannel.appendLine("----------------------START----------------------")
-		}).on('error', (error: any) => {
+
+			this.enabled = true
+			this.statusBarItem.command = "RemoteLogServerWithColorfulLog.stop_rls"
+			this.statusBarItem.text = "Remote Log Server: $(debug-stop)"
+			this.statusBarItem.tooltip = `Click the button to stop monitoring the log.\nHost:${host} Port:${port}`
+
+		}).once('error', (error: any) => {
 			// Failed to monitor
 			vscode.window.showErrorMessage(`Failed to start the Remote Log Server: ${error.message}`)
 			this.outputChannel.appendLine(`Error starting the server: ${error.message}`)
+			this.server.removeAllListeners("error").removeAllListeners("listening")
 		})
 	}
 
@@ -76,6 +81,7 @@ export class RemoteLogServer {
 		this.server.close()
 		// vscode.window.setStatusBarMessage('Log server stopped', 1000)
 		this.updateStatusBarTooltip()
+		this.server.removeAllListeners("error").removeAllListeners("listening")
 	}
 
 	public toggle() {
